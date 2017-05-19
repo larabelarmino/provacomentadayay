db1c2a49c44e2b80ed58f56e5493ba9a265de2b1
larabelarmino <lara.cbdm@gmail.com>
2017-05-19 14:56:47 -0300

    HAHAY

--- Solucao.js
+++ Solucao.js
@@ -1,55 +1,59 @@
-var fs   = require('fs')
-var file = process.argv[2]
-var extensao = file.split('.')[file.split('.').length-1]
-var numeros = [];
+var fs   = require('fs') // FS(FILESYSTEM) É UMA BIBLIOTECA JAVASCRIPT PARA GERENCIAMENTO DE ARQUIVOS. AO FAZERMOS QUALQUER OPERAÇÃO COM ARQUIVOS EXTERNOS, PRECISAMOS CRIAR UM OBJETO FS
+var file = process.argv[2] // PROCESS.ARGV PEGA OS ARGUMENTOS INSERIDOS AO EXECUTAR O ARQUIVO. FUNCIONA COMO UM VETOR. AS POSIÇÕES SÃO [NODE, NOMEDOARQUIVO, ...]. PEGANDO A POSIÇÃO DE NUMERO 2, PEGAMOS A PRIMEIRA PALAVRA DEPOIS DO NOME DO ARQUIVO
+var extensao = file.split('.')[file.split('.').length-1] //FILE(VARIVAVEL COM NOME DO ARQUIVO) SPLIT(FUNÇÃO PRA CORTAR STRINGS)
+//->ARGUMENTO INSERIDO: VAI CORTAR A PARTIR DO PRIMEIRO '.', ATE O FIM. ->ESTAMOS PEGANDO A EXTENSÃO DO ARQUIVO
+var numeros = []; //CRIANDO O VETOR QUE ARMAZENARÁ A RESPOSTA
 
-function json()
+function json() //FUNÇÃO PARA CASO A VARIAVEL EXTENSÃO SEJA "json"
 {
-  fs.readFile(__dirname + "/" + file, 'utf8', function(err, data)  {
-    numeros = JSON.parse(data);
-    numeros = numeros.valores;
-    Processar();
-  });
+//(EMBAIXO) OBJETO FS SENDO CHAMADO COM SEU MÉTODO READFILE(USADA PARA LER ARQUIVOS E PASSADA COM CERTOS PARAMETROS)
+//O MÉTODO READFILE É CHAMADO: "readFile(nomearquivo, codificação(opcional), função(argumentos){codigo})
+  fs.readFile(__dirname + "/" + file, 'utf8', function(err, data)  { //TA LENDO O ARQUIVO. O RESULTADO É A VARIAVEL "data"
+    //JSON.parse(argumento) transforma o argumento passado em json
+    numeros = JSON.parse(data); //DEFINE O VETOR COMO UM OBJETO CONVERTIDO DO VALOR OBTIDO DA LEITURA(a variavel "data" declarada na função)
+    numeros = numeros.valores; //DEFINE O VETOR COMO O VETOR(ATRIBUTO) QUE ESTÁ DENTRO DO ARQUIVO(no caso, nomeado como "valores")
+    Processar(); //CHAMA A FUNÇÃO PRINCIPAL
+  });//VARIAVEL "data" deletada após o fim da função
 }
-function csv()
+function csv() //FUNÇÃO PARA CASO A VARIAVEL EXTENSÃO SEJA "csv"
 {
-	var csv = require('csv-string');
-	var resul;
+	var csv = require('csv-string'); //csv-string É UMA BIBLIOTECA JAVASCRIPT(QUE PRECISA SER INSTALADA). É USADA PARA MANIPULAÇÃO DE ARQUIVOS CSV
+	var resul; //VARIAVEL PARA ARMAZENAR O RESULTADO DA LEITURA
 
 	fs.readFile(__dirname + "/" + file, 'utf8', function(err, data)  {
-    resul = csv.parse(data);
-    numeros  = resul[0];
+    resul = csv.parse(data); //DEFINE A VARIAVEL COM O RESULTADO DA LEITURA E CONVERTE PARA CSV
+//****CSV TEM AS POSIÇÕES[0,1,2,...] DEFINIDAS PELAS LINHAS DO ARQUIVO. **no nosso código há apenas uma linha
+    numeros  = resul[0]; //DEFINE O VETOR COMO A PRIMEIRA(e no nosso caso, a única) POSIÇÃO DA VARIÁVEL RESUL
     Processar();
 	});
 	
 }
-function xml()
+function xml() //FUNÇÃO PARA CASO A VARIAVEL EXTENSÃO SEJA "xml"
 {
-	var xmlParser = require('xml2js').parseString;
+	var xmlParser = require('xml2js').parseString;//xml2js É UMA BIBLIOTECA JAVASCRIPT(QUE PRECISA SER INSTALADA). É USADA PARA MANIPULAÇÃO DE ARQUIVOS XML
 
     fs.readFile(__dirname + "/" + file, function(err, data) {
-        xmlParser(data, function(err, result)
+        xmlParser(data, function(err, result)//NÃO SEI O QUE É ISSO. PORÉM PELA LÓGICA, É UMA FUNÇÃO PRA ADAPTAR O RESULTADO
         	{
-        		numeros = result.numeros.split(',');
+        		numeros = result.numeros.split(',');//DIVIDE O RESULTADO EM PARTES E ATRIBUI ISSO AO VETOR
             Processar();
         	});
     });
     
 }
+//CHECANDO A VARIAVEL EXTENSÃO
 if(extensao == 'json')
-	json();
+	json();//CHAMA A FUNÇÃO CASO JSON
 if(extensao == 'csv')
-	csv();
+	csv();//CHAMA A FUNÇÃO CASO CSV
 if(extensao == 'xml')
-	xml();
+	xml();//CHAMA A FUNÇÃO CASO XML
 
 
-function Processar()
+function Processar() // FUNÇÃO QUE VAI PROCESSAR TODO O CÓDIGO. CHAMADA A PARTIR DE UMA DAS FUNÇÕES DE EXTENSÕES.
 {
-  numeros = numeros.sort(function(a, b){return a-b});
-  console.log(numeros.length)
-var inicio = numeros[0];
-
+  numeros = numeros.sort(function(a, b){return a-b});//ORGANIZA OS NÚMEROS
+var inicio = numeros[0];//DEFINE UMA VARIÁVEL PARA OS INICIOS DOS INTERVALOS E A ATRIBUI COMO A PRIMEIRA POSIÇÃO DO VETOR
 var fim = [];
 var resultado = "";
 for(var i = 0; i < numeros.length;  i++)
